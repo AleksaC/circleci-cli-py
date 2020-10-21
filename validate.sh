d1ab1df3b8489f8ca80e10c931d2ec0539661439
CircleCI <circleci@f3c520b125d8>
2020-10-21 12:01:28 +0000

    Update to version v0.1.11113

--- validate.sh
+++ validate.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 BINARY="${DIR}/circleci"
 ARCHIVE="${BINARY}.tar.gz"
-VERSION="0.1.11062"
+VERSION="0.1.11113"
 
 BASE_URL="https://github.com/CircleCI-Public/circleci-cli/releases/download/v${VERSION}"
 case $OSTYPE in
