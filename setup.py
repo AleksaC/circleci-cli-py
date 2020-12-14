b61e95663169eabac48a33f139a31013e0187b82
AleksaC <aleksacukovic1@gmail.com>
2020-12-15 00:29:11 +0100

    Add python package wrapper

---
+++ setup.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+import hashlib
+import http
+import io
+import os.path
+import stat
+import sys
+import tarfile
+import urllib.request
+import zipfile
+from distutils.command.build import build as orig_build
+from distutils.core import Command
+
+from setuptools import setup
+from setuptools.command.install import install as orig_install
+
+CIRCLECI_CLI_VERSION = '0.1.11508'
+POSTFIX_SHA256 = {
+    'linux': (
+        'circleci-cli_0.1.11508_linux_amd64.tar.gz',
+        '136463ba2bd8f7d0b9486b02c2c71dbb7a66602e3910bc96afdc641a1101ce7c',
+    ),
+    'darwin': (
+        'circleci-cli_0.1.11508_darwin_amd64.tar.gz',
+        'ebeecb75485eb29da9da444987d4a499f5fec974d8236e9a5718dd598c2faf2b',
+    ),
+    'win32': (
+        'circleci-cli_0.1.11508_windows_amd64.zip',
+        'afd959c57deb5869886c5e0ee3bbdfed066b0009ba769839de2662935f9f3264',
+    ),
+}
+PY_VERSION = '1'
+
+
+def get_download_url() -> str:
+    postfix, sha256 = POSTFIX_SHA256[sys.platform]
+    url = (
+        f'https://github.com/CircleCI-Public/circleci-cli/releases/download/'
+        f'v{CIRCLECI_CLI_VERSION}/{postfix}'
+    )
+    return url, sha256
+
+
+def download(url: str, sha256: str) -> bytes:
+    with urllib.request.urlopen(url) as resp:
+        code = resp.getcode()
+        if code != http.HTTPStatus.OK:
+            raise ValueError(f'HTTP failure. Code: {code}')
+        data = resp.read()
+
+    checksum = hashlib.sha256(data).hexdigest()
+    if checksum != sha256:
+        raise ValueError(f'sha256 mismatch, expected {sha256}, got {checksum}')
+
+    return data
+
+
+def extract(url: str, data: bytes) -> bytes:
+    with io.BytesIO(data) as bio:
+        if '.tar.' in url:
+            with tarfile.open(fileobj=bio) as tarf:
+                for info in tarf.getmembers():
+                    if info.isfile() and info.name.endswith('circleci'):
+                        return tarf.extractfile(info).read()
+        elif url.endswith('.zip'):
+            with zipfile.ZipFile(bio) as zipf:
+                for info in zipf.infolist():
+                    if info.filename.endswith('.exe'):
+                        return zipf.read(info.filename)
+
+    raise AssertionError(f'unreachable {url}')
+
+
+def save_executable(data: bytes, base_dir: str):
+    exe = 'circleci' if sys.platform != 'win32' else 'circleci.exe'
+    output_path = os.path.join(base_dir, exe)
+    os.makedirs(base_dir)
+
+    with open(output_path, 'wb') as fp:
+        fp.write(data)
+
+    # Mark as executable.
+    # https://stackoverflow.com/a/14105527
+    mode = os.stat(output_path).st_mode
+    mode |= stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
+    os.chmod(output_path, mode)
+
+
+class build(orig_build):
+    sub_commands = orig_build.sub_commands + [('fetch_binaries', None)]
+
+
+class install(orig_install):
+    sub_commands = orig_install.sub_commands + [('install_shellcheck', None)]
+
+
+class fetch_binaries(Command):
+    build_temp = None
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        self.set_undefined_options('build', ('build_temp', 'build_temp'))
+
+    def run(self):
+        # save binary to self.build_temp
+        url, sha256 = get_download_url()
+        archive = download(url, sha256)
+        data = extract(url, archive)
+
+        save_executable(data, self.build_temp)
+
+
+class install_shellcheck(Command):
+    description = 'install the shellcheck executable'
+    outfiles = ()
+    build_dir = install_dir = None
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        # this initializes attributes based on other commands' attributes
+        self.set_undefined_options('build', ('build_temp', 'build_dir'))
+        self.set_undefined_options(
+            'install', ('install_scripts', 'install_dir'),
+        )
+
+    def run(self):
+        self.outfiles = self.copy_tree(self.build_dir, self.install_dir)
+
+    def get_outputs(self):
+        return self.outfiles
+
+
+command_overrides = {
+    'install': install,
+    'install_shellcheck': install_shellcheck,
+    'build': build,
+    'fetch_binaries': fetch_binaries,
+}
+
+
+try:
+    from wheel.bdist_wheel import bdist_wheel as orig_bdist_wheel
+except ImportError:
+    pass
+else:
+    class bdist_wheel(orig_bdist_wheel):
+        def finalize_options(self):
+            orig_bdist_wheel.finalize_options(self)
+            # Mark us as not a pure python package
+            self.root_is_pure = False
+
+        def get_tag(self):
+            _, _, plat = orig_bdist_wheel.get_tag(self)
+            # We don't contain any python source, nor any python extensions
+            return 'py2.py3', 'none', plat
+
+    command_overrides['bdist_wheel'] = bdist_wheel
+
+setup(version=f'{CIRCLECI_CLI_VERSION}.{PY_VERSION}', cmdclass=command_overrides)
